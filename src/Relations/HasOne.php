7ecfdc61b76bef23b376360219efac66e05b1c74
Jérôme Tamarelle <jerome.tamarelle@mongodb.com>
2025-01-09 18:31:47 +0100

    -

--- src/Relations/HasOne.php
+++ src/Relations/HasOne.php
@@ -7,6 +7,9 @@ namespace MongoDB\Laravel\Relations;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne as EloquentHasOne;
+use Illuminate\Support\Arr;
+
+use function is_null;
 
 class HasOne extends EloquentHasOne
 {
@@ -36,6 +39,49 @@ class HasOne extends EloquentHasOne
         $foreignKey = $this->getForeignKeyName();
 
         return $query->select($foreignKey)->where($foreignKey, 'exists', true);
+    }
+
+    public function ofMany($column = 'id', $aggregate = 'MAX', $relation = null)
+    {
+        return parent::ofMany($column, $aggregate, $relation);
+        $this->ofMany = true;
+
+        $order = match($aggregate) {
+            'MAX' => -1,
+            'MIN' => 1,
+            default => throw new \LogicException('Invalid aggregate fonction for ofMany relation. Available aggregates: MIN, MAX'),
+        };
+    }
+
+    protected function newOneOfManySubQuery($groupBy, $columns = null, $aggregate = null)
+    {
+        $subQuery = $this->query->getModel()
+            ->newQuery()
+            ->withoutGlobalScopes($this->removedScopes());
+
+        foreach (Arr::wrap($groupBy) as $group) {
+            $subQuery->groupBy($this->qualifyRelatedColumn($group));
+        }
+
+        if (! is_null($columns)) {
+            foreach ($columns as $key => $column) {
+                $aggregatedColumn = $subQuery->qualifyColumn($column);
+
+                if ($key === 0) {
+                    $aggregatedColumn = ['$' . strtolower($aggregate) => '$' . $aggregatedColumn];
+                } else {
+                    $aggregatedColumn = ['$min' => '$' . $aggregatedColumn];
+                }
+
+                $subQuery->project([$column . '_aggregate' => $aggregatedColumn]);
+            }
+        }
+
+        $this->addOneOfManySubQueryConstraints($subQuery, column: null, aggregate: $aggregate);
+
+        return $subQuery;
+
+        parent::newOneOfManySubQuery($groupBy, $columns, $aggregate);
     }
 
     /**
